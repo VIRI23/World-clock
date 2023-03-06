fcbed1ba363b123fff4bf0f07d6e25fb940836f0
VIRI23 <virianyc3@gmail.com>
2023-03-06 13:38:06 -0700

    updates single location with time and date in js

---
+++ JavaScript/index.js
@@ -0,0 +1,54 @@
+function updateTime() {
+  // El Paso
+  let elPasoElement = document.querySelector("#el-paso");
+  if (elPasoElement) {
+    let elPasosDateElement = elPasoElement.querySelector(".date");
+    let elPasoTimeElement = elPasoElement.querySelector(".time");
+    let elPasoTime = moment().tz("America/El_Paso");
+
+    elPasosDateElement.innerHTML = elPasoTime.format("MMMM	Do YYYY");
+    elPasoTimeElement.innerHTML = elPasoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  // Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM	Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
